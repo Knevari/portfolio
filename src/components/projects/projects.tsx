ad3584bfcf92a720a21ebea64fbff224ece01e40
Knevari <winresource000@gmail.com>
2023-11-19 22:23:33 -0300

    perf: play video on hover

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -1,11 +1,50 @@
 import { useTranslation } from "react-i18next";
 
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import Project from "./project";
 
 import ScrollControls from "../common/scroll-controls";
 
 import { projects } from "./data.json";
+
+const ProjectVideo = ({ src }: { src: string }) => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    const video = videoRef.current;
+    if (!video) return;
+
+    const onMouseOver = () => {
+      video.play();
+    };
+
+    const onMouseOut = () => {
+      video.pause();
+    };
+
+    video.addEventListener("mouseenter", onMouseOver);
+    video.addEventListener("mouseout", onMouseOut);
+    return () => {
+      video.removeEventListener("mouseenter", onMouseOver);
+      video.removeEventListener("mouseout", onMouseOut);
+    };
+  }, []);
+
+  return (
+    <video
+      ref={videoRef}
+      width="100%"
+      height="100%"
+      className="absolute h-full w-full object-contain"
+      autoPlay
+      muted
+      loop
+    >
+      <source src={src} type="video/mp4" />
+      Your browser does not support the video tag.
+    </video>
+  );
+};
 
 export default function Projects() {
   const { t } = useTranslation();
@@ -27,23 +66,12 @@ export default function Projects() {
             href={project.source}
             className="group relative md:min-w-[350px]"
           >
-            <div className="absolute left-0 top-0 h-full w-full bg-gradient-to-br from-gray to-grayer">
-              {project.video && (
-                <video
-                  width="100%"
-                  height="100%"
-                  className="absolute h-full w-full object-contain"
-                  muted
-                  loop
-                >
-                  <source src={project.video} type="video/mp4" />
-                  Your browser does not support the video tag.
-                </video>
-              )}
+            <div className=" left-0 top-0 h-full w-full bg-gradient-to-br from-gray to-grayer">
+              {project.video && <ProjectVideo src={project.video} />}
               <img
                 src={project.thumbnail}
                 alt={project.name}
-                className="absolute left-0 top-0 h-full w-full object-contain transition delay-100 duration-500 group-hover:opacity-0"
+                className="absolute left-0 top-0 h-full min-h-[250px] w-full object-contain transition delay-100 duration-500 group-hover:opacity-0"
               />
               <div className="absolute bottom-0 w-full bg-gradient-to-br from-gray to-grayer px-4 py-2">
                 <p className="font-semibold text-white">{project.name}</p>
